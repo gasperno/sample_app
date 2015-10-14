79274b02aaf5a1a24a50997bd917867c946a1e58
Harsha <harsha049@gmail.com>
2015-10-14 21:19:43 +0000

    Make a basic model(including passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,11 +2,24 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name:"Example User",email:"example@user.nect")
+    @user = User.new(name:"Example User",email:"example@user.nect",
+                     password:"foobar",password_confirmation: "foobar")
   end
+  
+  
   
   test "should be valid" do
     assert @user.valid?
+  end
+  
+  test "password should be present(nonblank)" do
+    @user.password=@user.password_confirmation=" "*6
+    assert_not @user.valid?
+  end
+  
+  test "password should have minimum length" do
+    @user.password=@user.password_confirmation="a"*5
+    assert_not @user.valid?
   end
   
   test "user name should be present" do
